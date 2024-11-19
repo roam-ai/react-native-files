211fc00fea16ad75a33337d149f1186f7df3b6f4
Adhamsheriff-Roam <sheriff@roam.ai>
2024-11-19 15:45:10 +0530

    Update RNRoam.h

--- ios/RNRoam.h
+++ ios/RNRoam.h
@@ -4,14 +4,18 @@
 //
 
 #import <Foundation/Foundation.h>
+// Conditional import for compatibility with different React Native versions
 
 #if __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
 #else
 #import <React/RCTBridgeModule.h>
 #endif
+
+// Import the RCTEventEmitter protocol for emitting events to the JavaScript side
 #import <React/RCTEventEmitter.h>
 
+// Declare the RNRoam class, inheriting from RCTEventEmitter and RCTBridgeModule
 @interface RNRoam : RCTEventEmitter <RCTBridgeModule>
 
 @end
